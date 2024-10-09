b60de364b55df54c94d6997016df258a6ebc2a47
Gerardo sanchez gonzalez <gerardo.sanchez.con@gmail.com>
2024-10-09 13:52:20 -0600

    avance

--- PreExamen.py
+++ PreExamen.py
@@ -1,62 +1,21 @@
 import cv2
 import numpy as np
 
-def dibujarContorno(contornos, color):
-    for (i, c) in enumerate(contornos):
-        M = cv2.moments(c)
-        if M["m00"] != 0: 
-            x = int(M["m10"]/M["m00"])
-            y = int(M["m01"]/M["m00"])
-            cv2.drawContours(imagen, [c], 0, color, 2)
-            cv2.putText(imagen, str(i+1), (x-10, y+10), 1, 2, (0, 0, 0), 2)
-        else:
-            cv2.drawContours(imagen, [c], 0, color, 2)
-
-amarilloBajo = np.array([20, 100, 20], np.uint8)
-amarilloAlto = np.array([32, 255, 255], np.uint8)
-
-verdeBajo = np.array([36, 100, 20], np.uint8)
-verdeAlto = np.array([70, 255, 255], np.uint8)
-
-rojoBajo1 = np.array([0, 100, 20], np.uint8)
-rojoAlto1 = np.array([10, 255, 255], np.uint8)
-rojoBajo2 = np.array([175, 100, 20], np.uint8)
-rojoAlto2 = np.array([180, 255, 255], np.uint8)
-
 imagen = cv2.imread('salida.png')
+
 imagenHSV = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
 
-#Detectando colores 
-maskAmarillo = cv2.inRange(imagenHSV, amarilloBajo, amarilloAlto)
-maskVerde = cv2.inRange(imagenHSV, verdeBajo, verdeAlto)
-maskRojo1 = cv2.inRange(imagenHSV, rojoBajo1, rojoAlto1)
-maskRojo2 = cv2.inRange(imagenHSV, rojoBajo2, rojoAlto2)
-maskRojo =  cv2.add(maskRojo1, maskRojo2)
+# Rangos de colores
+amarilloBajo = np.array([20, 100, 100], np.uint8)
+amarilloAlto = np.array([30, 255, 255], np.uint8)
 
-#Encontrando contornos
-contornosAmarillo = cv2.findContours(maskAmarillo, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-contornosVerde = cv2.findContours(maskVerde, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-contornosRojo = cv2.findContours(maskRojo, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+verdeBajo = np.array([36, 100, 100], np.uint8)
+verdeAlto = np.array([70, 255, 255], np.uint8)
 
-dibujarContorno(contornosAmarillo, (0, 255,255))
-dibujarContorno(contornosVerde, (0, 255, 0))
-dibujarContorno(contornosRojo, (0, 0, 255))
+rojoBajo1 = np.array([0, 100, 100], np.uint8)
+rojoAlto1 = np.array([10, 255, 255], np.uint8)
+rojoBajo2 = np.array([160, 100, 100], np.uint8)
+rojoAlto2 = np.array([180, 255, 255], np.uint8)
 
-#Imagen Resumen
-imgResumen = 255 * np.ones((210,100,3), dtype = np.uint8)
-cv2.circle(imgResumen, (30,30), 15, (0,255,255), -1)
-cv2.circle(imgResumen, (30,70), 15, (140,40,120), -1)
-cv2.circle(imgResumen, (30,110), 15, (0,255,0), -1)
-cv2.circle(imgResumen, (30,150), 15, (0,0,255), -1)
-
-cv2.putText(imgResumen,str(len(contornosAmarillo)),(65,40), 1, 2,(0,0,0),2)
-cv2.putText(imgResumen,str(len(contornosVerde)),(65,120), 1, 2,(0,0,0),2)
-cv2.putText(imgResumen,str(len(contornosRojo)),(65,160), 1, 2,(0,0,0),2)
-totalCnts = len(contornosAmarillo) + len(contornosVerde) + len(contornosRojo)
-cv2.putText(imgResumen,str(totalCnts),(55,200), 1, 2,(0,0,0),2)
-cv2.imshow('Resumen', imgResumen)
-
-cv2.imshow('Imagen', imagen)
-cv2.imwrite('conteo.png', imagen)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+azulBajo = np.array([100, 100, 100], np.uint8)
+azulAlto = np.array([130, 255, 255], np.uint8)
